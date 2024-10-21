403c2362cdfaa727ab694fce0f43bfe231460981
Leon.Liechti <gitlon@proton.me>
2024-10-21 15:52:14 +0200

    init

---
+++ src/main/java/com/lon60/keycloakspringboottemplate/KeycloakSpringBootTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.lon60.keycloakspringboottemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakSpringBootTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakSpringBootTemplateApplication.class, args);
+    }
+
+}
